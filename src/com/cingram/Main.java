7c3c11ce1962089f86806c5f26205d2f19d5d7d7
CIngram82 <cingram82@gmail.com>
2018-01-23 08:21:21 -0500

    first commit

---
+++ src/com/cingram/Main.java
@@ -0,0 +1,10 @@
+package com.cingram;
+
+public class Main {
+    public static void main (String[] args){
+        Car leaf = new Car();
+        leaf.setModel("Leaf 2015");
+        System.out.println("Model is " + leaf.getModel());
+
+    }
+}
